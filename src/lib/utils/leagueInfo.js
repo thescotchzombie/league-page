5fbefba20e40e18d1cf20c28dde42424109c690b
thescotchzombie <89694486+thescotchzombie@users.noreply.github.com>
2021-08-28 09:39:24 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "699269785296388096"; // your league ID
+export const leagueName = "The League of Ballbreakers"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
